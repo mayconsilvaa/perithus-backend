268b08e68ae208e7650a220530d2cf7a2331176d
Maycon Silva <msmaxhost@gmail.com>
2020-05-08 11:27:40 -0300

    addport

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import app from './app';
 
-app.listen(process.env.SV_PORT, () => {
+app.listen(process.env.SV_PORT || 3333, () => {
   console.log('Server Online na porta:', process.env.SV_PORT);
 });
